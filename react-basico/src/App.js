fb01d706c4e4789bdd0b23f581500eee2c664973
Joao Pedro <802.018@alunos.unigran.br>
2020-10-29 14:46:03 -0300

    Criando Componentes

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Comentario from './components/Comentarios';
+
+// para iniciar o server npm start
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meu primeiro projeto</h1>
+      <Comentario/>
+      <Comentario/>
+      <Comentario/>
     </div>
   );
 }
---
+++ react-basico/src/components/Comentarios.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//importar o css
+import './Comentario.css';
+
+const Comentario = () =>(
+    <div className="Comentario">
+         <div>Ola eu sou um compenente</div>
+            João
+            </div>
+);
+
+export default Comentario;
